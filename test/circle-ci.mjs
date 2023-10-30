44ab7606c7b3559dd81947b55e013ddd6be1d974
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2023-10-30 01:58:23 +0100

    add tests

--- test/circle-ci.mjs
+++ test/circle-ci.mjs
@@ -17,12 +17,12 @@ try {
     const test = 'B62qmKTWum3TYe3HSDKCK6NWkMhNyjPJbwCD6HuD3ypvSWw5drL8D1x'
     if( result[ 1 ]['data']['transaction']['to'] === test ) {
         console.log( `Success! data.transaction.to === "${test}"` )
-        process.exit( 1 )
+        process.exit( 0 )
     } else {
         console.log( `Failed! Transaction key "to" is not equal ${test}` )
-        process.exit( 0 )
+        process.exit( 1 )
     }
 } catch( e ) {
     console.log( `Failed! Parsing error, received wrong payload. ${e}` )
-    process.exit( 0 )
+    process.exit( 1 )
 }
